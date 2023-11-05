25a4914dd9c3a2c4760ee1fffed3709b1db82191
MarketaHorakova <99902734+MarketaHorakova@users.noreply.github.com>
2023-11-05 16:20:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace HW1_Min_Max_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("How many elements for your array: ");
+            int delkaPole;
+            while (!int.TryParse(Console.ReadLine(), out delkaPole))
+            {
+                Console.WriteLine("Invalit input, try it again:");
+            }
+
+            int[] poleCisel = new int[delkaPole];
+            for (int i = 0; i < poleCisel.Length; i++)
+            {
+                poleCisel[i] = random.Next(0, 1000);
+            }
+
+            Console.WriteLine($"here is the array: ");
+
+            for (int i = 0; i < poleCisel.Length; i++)
+            {
+                Console.Write($"{poleCisel[i]} ");
+            }
+
+            int sumMin = poleCisel.Sum() - poleCisel.Max();
+            int sumMax = poleCisel.Sum() - poleCisel.Min();
+
+            Console.WriteLine($"\nMinimal summary is {sumMin} and maximal summary is {sumMax}.");
+            Console.ReadLine();
+        }
+    }
+}
